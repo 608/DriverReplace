0c7dcb7753680b8c0e83bd1ff9d08f002c2f47d8
BZL <608@users.noreply.github.com>
2024-06-20 23:51:40 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DriverReplace
+{
+    internal class Program
+    {
+        [DllImport("libwdi.dll", EntryPoint = "DriverReplace")]
+        public static extern void DriverReplace(int vid, int pid);
+        static void Main(string[] args)
+        {
+            Thread.GetDomain().SetPrincipalPolicy(PrincipalPolicy.WindowsPrincipal);
+            var pri = (WindowsPrincipal)Thread.CurrentPrincipal;
+            
+            if (!pri.IsInRole(WindowsBuiltInRole.Administrator))
+            {
+                var proc = new ProcessStartInfo()
+                {
+                    WorkingDirectory = Environment.CurrentDirectory,
+                    FileName = Assembly.GetEntryAssembly().Location,
+                    Verb = "RunAs",
+                    Arguments = string.Join(" ", args)
+                };
+                
+                Process.Start(proc);
+                return;
+            }
+
+            var vid = int.Parse(args[0]);
+            var pid = int.Parse(args[1]);
+            DriverReplace(vid, pid);
+        }
+    }
+}
